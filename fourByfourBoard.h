99511e78eaeacce8503139f6f670d74ede6c97db
hassan-sameh <hs992374@gmail.com>
2025-11-26 17:00:58 +0200

    Create fourByfourBoard.h

---
+++ fourByfourBoard.h
@@ -0,0 +1,18 @@
+#ifndef FOUR_BY_FOUR_BOARD_H
+#define FOUR_BY_FOUR_BOARD_H
+
+#include "BoardGame_Classes.h"
+#include "FourByFourMove.h"
+
+class FourByFourBoard : public Board<char> {
+public:
+    FourByFourBoard();
+
+    bool update_board(Move<char>* move) override;
+    bool is_win(Player<char>* p) override;
+    bool is_lose(Player<char>* p) override;
+    bool is_draw(Player<char>* p) override;
+    bool game_is_over(Player<char>* p) override;
+};
+
+#endif
