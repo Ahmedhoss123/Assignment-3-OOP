a17745480b1ffe3119888f5dcb34130760fbf4cc
hassan-sameh <hs992374@gmail.com>
2025-12-06 23:20:44 +0200

    Update AIPlayer.h

--- AIPlayer.h
+++ AIPlayer.h
@@ -1,12 +1,12 @@
 #pragma once
-#include "Player.h"
+#include "BoardGame_Classes.h"
 #include "FourByFourBoard.h"
 #include "FourByFourMove.h"
 
 class AIPlayer : public Player<char> {
 public:
-    AIPlayer(char symbol);
-    Move<char>* get_move(Board<char>* b) override;
+    AIPlayer(string name, char symbol);
+    Move<char>* get_move(Board<char>* b);
 
 private:
     int minimax(FourByFourBoard* board, int depth, bool isMax);
