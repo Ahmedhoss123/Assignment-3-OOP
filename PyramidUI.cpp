f442f33894e4c92e1eb10e9a3c0ea84ec36ec169
hassan-sameh <hs992374@gmail.com>
2025-12-08 01:15:08 +0200

    Update PyramidUI.cpp

--- PyramidUI.cpp
+++ PyramidUI.cpp
@@ -1,14 +1,33 @@
 #include "PyramidUI.h"
+#include "PyramidMove.h"
+#include "PyramidAI.h"
+#include <iostream>
+using namespace std;
 
 PyramidUI::PyramidUI() : UI<char>("Pyramid Tic Tac Toe", 3) {}
 
 Move<char>* PyramidUI::get_move(Player<char>* player) {
-    int x,y;
+
+    
+    if (player->get_type() == PlayerType::COMPUTER) {
+        return player->get_move();
+    }
+
+   
+    int x, y;
     cout << player->get_name() << " enter cell (row col): ";
     cin >> x >> y;
-    return new Move<char>(x,y,player->get_symbol());
+
+    return new PyramidMove<char>(x, y, player->get_symbol());
 }
 
 Player<char>* PyramidUI::create_player(string& name, char symbol, PlayerType type) {
+
+   
+    if (type == PlayerType::COMPUTER) {
+        return new PyramidAI(name, symbol);
+    }
+
+    
     return new Player<char>(name, symbol, type);
 }
