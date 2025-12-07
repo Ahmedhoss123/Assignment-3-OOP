7d03d2e68951def811644041c46c9e42c0329186
hassan-sameh <hs992374@gmail.com>
2025-12-08 01:08:42 +0200

    Update PyramidBoard.cpp

--- PyramidBoard.cpp
+++ PyramidBoard.cpp
@@ -15,7 +15,9 @@ bool PyramidBoard::update_board(Move<char>* move) {
     int y = move->get_y();
     char s = move->get_symbol();
 
+    if (x < 0 || x >= 3 || y < 0 || y >= 5) return false;
     if(board[x][y] != '.') return false;
+
     board[x][y] = s;
     n_moves++;
     return true;
@@ -24,12 +26,12 @@ bool PyramidBoard::update_board(Move<char>* move) {
 bool PyramidBoard::is_win(Player<char>* p) {
     char s = p->get_symbol();
 
-    // أفقي
+    // أفقي (الصف الأخير فقط)
     for(int j=0;j<=2;j++)
-        if(board[2][j]==s && board[2][j+1]==s && board[2][j+2]==s) return true;
+        if(board[2][j]==s && board[2][j+1]==s && board[2][j+2]==s)
+            return true;
 
     // عمودي
-    if(board[0][0]==s && board[1][1]==s && board[2][2]==s) return true;
     if(board[0][0]==s && board[1][0]==s && board[2][0]==s) return true;
 
     // قطري
@@ -39,8 +41,28 @@ bool PyramidBoard::is_win(Player<char>* p) {
     return false;
 }
 
-bool PyramidBoard::is_lose(Player<char>*) { return false; }
-bool PyramidBoard::is_draw(Player<char>*) { return n_moves == 9; }
+bool PyramidBoard::is_lose(Player<char>*) {
+    return false;
+}
+
+bool PyramidBoard::is_draw(Player<char>*) {
+    return n_moves == 9;
+}
+
 bool PyramidBoard::game_is_over(Player<char>* p) {
     return is_win(p) || is_draw(p);
+}
+
+   // AI (Minimax + Backtracking)
+   
+
+void PyramidBoard::undo_move(Move<char>* move) {
+    int x = move->get_x();
+    int y = move->get_y();
+    board[x][y] = '.';
+    n_moves--;
+}
+
+char PyramidBoard::get_cell(int r, int c) const {
+    return board[r][c];
 }
