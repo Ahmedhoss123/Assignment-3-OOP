4cbdb8f6c3d24b54b40aeac23645b908c5707124
Abdallah Ahmed <abdallah.ab1492006@gmail.com>
2025-12-11 01:59:49 +0200

    Update XO4R_UI.cpp

--- XO4R_UI.cpp
+++ XO4R_UI.cpp
@@ -6,23 +6,64 @@ using namespace std;
 XO4R_UI::XO4R_UI() : UI<char>("Welcome to Connect Four (6x7) by Dr El-Ramly", 3) {}
 
 Player<char>* XO4R_UI::create_player(string& name, char symbol, PlayerType type) {
-    // Create player based on type
-    cout << "Creating " << (type == PlayerType::HUMAN ? "human" : "computer")
-        << " player: " << name << " (" << symbol << ")\n";
-
+    if (type == PlayerType::AI) {
+        cout << "Perfect Unbeatable AI activated!\n";
+        return new XO4R_AI_Player(name, symbol);
+    }
     return new Player<char>(name, symbol, type);
 }
 
 Move<char>* XO4R_UI::get_move(Player<char>* player) {
-    int x, y;
+    // Special case: Perfect AI player
+    if (player->get_type() == PlayerType::AI) {
+        XO4R_AI_Player* ai = static_cast<XO4R_AI_Player*>(player);
+        XO4R_Board* board = static_cast<XO4R_Board*>(player->get_board_ptr());
+        return ai->make_perfect_move(board);
+    }
+
+    // Human or Random
+    int x = -1, y = -1;
+	auto b = player->get_board_ptr()->get_board_matrix();
 
     if (player->get_type() == PlayerType::HUMAN) {
-        cout << "\nPlease enter your move x (0 to 5) and y (0 to 6): ";
-        cin >> x >> y;
+        while (true) {
+			cout << "\n" << player->get_name() << " (" << player->get_symbol() << "). Enter column (0-6): ";
+			if (!(cin >> y)) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid input. Please enter an integer between 0 and 6.\n";
+                continue;
+            }
+
+            if (y >= 0 && y < 7) {
+				bool full = true;
+                for (int r = 5; r >= 0; --r) {
+                    if (b[r][y] == '.') {
+                        x = r;
+                        full = false;
+                        break;
+                    }
+                }
+                if (!full) break;
+                else cout << "Column is full! Try another.\n";
+            }
+            else {
+                cout << "Invalid column. Please enter a number between 0 and 6.\n";
+			}
+		}
     }
-    else if (player->get_type() == PlayerType::COMPUTER) {
-        x = rand() % player->get_board_ptr()->get_rows();
-        y = rand() % player->get_board_ptr()->get_columns();
+    else {
+        do {
+			y = rand() % 7;
+            x = -1;
+            for (int r = 5; r >= 0; --r) {
+                if (b[r][y] == '.') {
+                    x = r;
+                    break;
+                }
+            }
+		} while (x == -1);
+        cout << "\nRandom computer plays column: " << y << endl;
     }
     return new Move<char>(x, y, player->get_symbol());
 }
