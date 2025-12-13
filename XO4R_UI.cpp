a1ad809b8c83dae7c2dcf2515edd9b14a1367cdc
Ahmedhoss123 <ahmedhamody120@gmail.com>
2025-12-13 13:49:22 +0200

    Enhance player creation to include COMPUTER type
    Added support for COMPUTER player type in player creation.

--- XO4R_UI.cpp
+++ XO4R_UI.cpp
@@ -6,8 +6,10 @@ using namespace std;
 XO4R_UI::XO4R_UI() : UI<char>("Welcome to Connect Four (6x7) by Dr El-Ramly", 3) {}
 
 Player<char>* XO4R_UI::create_player(string& name, char symbol, PlayerType type) {
-    if (type == PlayerType::AI) {
+    // Fix: Check for both AI and COMPUTER types
+    if (type == PlayerType::AI || type == PlayerType::COMPUTER) {
         cout << "Perfect Unbeatable AI activated!\n";
+        // This constructor sets the internal type to PlayerType::AI
         return new XO4R_AI_Player(name, symbol);
     }
     return new Player<char>(name, symbol, type);
@@ -23,12 +25,12 @@ Move<char>* XO4R_UI::get_move(Player<char>* player) {
 
     // Human or Random
     int x = -1, y = -1;
-	auto b = player->get_board_ptr()->get_board_matrix();
+    auto b = player->get_board_ptr()->get_board_matrix();
 
     if (player->get_type() == PlayerType::HUMAN) {
         while (true) {
-			cout << "\n" << player->get_name() << " (" << player->get_symbol() << "). Enter column (0-6): ";
-			if (!(cin >> y)) {
+            cout << "\n" << player->get_name() << " (" << player->get_symbol() << "). Enter column (0-6): ";
+            if (!(cin >> y)) {
                 cin.clear();
                 cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 cout << "Invalid input. Please enter an integer between 0 and 6.\n";
@@ -36,7 +38,7 @@ Move<char>* XO4R_UI::get_move(Player<char>* player) {
             }
 
             if (y >= 0 && y < 7) {
-				bool full = true;
+                bool full = true;
                 for (int r = 5; r >= 0; --r) {
                     if (b[r][y] == '.') {
                         x = r;
@@ -49,12 +51,12 @@ Move<char>* XO4R_UI::get_move(Player<char>* player) {
             }
             else {
                 cout << "Invalid column. Please enter a number between 0 and 6.\n";
-			}
-		}
+            }
+        }
     }
     else {
         do {
-			y = rand() % 7;
+            y = rand() % 7;
             x = -1;
             for (int r = 5; r >= 0; --r) {
                 if (b[r][y] == '.') {
@@ -62,7 +64,7 @@ Move<char>* XO4R_UI::get_move(Player<char>* player) {
                     break;
                 }
             }
-		} while (x == -1);
+        } while (x == -1);
         cout << "\nRandom computer plays column: " << y << endl;
     }
     return new Move<char>(x, y, player->get_symbol());
